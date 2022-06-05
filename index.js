6ee9b1f18377c31da732344b51b03b9ebcdfe315
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-05 08:50:28 +0300

    commit tree 9307f9a9314539545b34c78ceed724c5f60c9ec3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+ function writeCards(array,eventName){
+     let thankYouCards = [];
+      for ( let i = 0;i<array.length;i++){
+      thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+     }
+  return thankYouCards
+ }
+
+ function countDown(number){
+     while(number > 0){
+         console.log(number)
+         number --
+         
+     }
+     console.log(number);
+ }
+ 
